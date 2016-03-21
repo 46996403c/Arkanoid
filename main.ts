c754ebd6f0f7f87ea107989a4121b63cb0ddec91
oscar <46996403c@iespoblenou.org>
2016-03-21 07:51:57 +0100

    Initial commit, HTML modificado

---
+++ main.ts
@@ -0,0 +1,34 @@
+/// <reference path="phaser/phaser.d.ts"/>
+
+
+class mainState extends Phaser.State {
+    game: Phaser.Game;
+
+    preload():void {
+        super.preload();
+    }
+
+    create():void {
+        super.create();
+
+    }
+
+    update():void {
+        super.update();
+    }
+}
+
+class SimpleGame {
+    game:Phaser.Game;
+
+    constructor() {
+        this.game = new Phaser.Game(800, 581, Phaser.AUTO, 'gameDiv');
+
+        this.game.state.add('main', mainState);
+        this.game.state.start('main');
+    }
+}
+
+window.onload = () => {
+    var game = new SimpleGame();
+};
